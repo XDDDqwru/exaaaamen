d093751b2f14a1297ead61f5689fd0217dd04b5c
XDDDqwru <mmiau860@gmail.com>
2026-04-14 14:23:42 -0300

    Update exaaaamen.py

--- exaaaamen.py
+++ exaaaamen.py
@@ -1,6 +1,14 @@
 import pandas as pd
-df = pd.read_csv('StudentsPerformance.csv')     
-print(df[['math score', 'reading score', 'writing score']].head(10))
-print(df[['math score', 'reading score', 'writing score']].shape)
-print("Cantidad:", df['reading score'].count())
-print("Suma total:", df['reading score'].sum())
+df = pd.read_csv('StudentsPerformance.csv')
+
+print(df[['math score', 'reading score', 'writing score']])
+
+print(df[ 'writing score'].dtype)
+
+print(df[ 'writing score'].count())
+
+print(df.info())
+
+print(df.shape)
+
+print(df.tail())
